db621bd0e0923de904f81deebc3b0fc6d94eb715
Ubuntu <emi.myle@protonmail.com>
2025-10-06 22:52:00 +0200

    fixed model version

--- projects/excot_dpo/data_generation/data_generation.sh
+++ projects/excot_dpo/data_generation/data_generation.sh
@@ -12,6 +12,6 @@ cd projects/excot_dpo/
 python data_generation/data_generation.py \
     --config-path data_generation/configs/bird_config.yaml \
     --type vllm \
-    --model-name Qwen/Qwen2.5-32B-Instruct \
+    --model-name Qwen/Qwen2.5-0.5B-Instruct \
     --vllm-output-path output \
     --tp-size 1
